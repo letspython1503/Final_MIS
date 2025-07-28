d88e995ef367250f9bb789dcfde0f284b982ee4b
letspython1503 <gbrijeshmukunda@gmail.com>
2025-07-28 16:55:15 +0530

    Fix: use app.run instead of deprecated run_server

--- mis_dashapp/app.py
+++ mis_dashapp/app.py
@@ -105,4 +105,4 @@ def layout():
 app.layout = layout()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run_server(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
